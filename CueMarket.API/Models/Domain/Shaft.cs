18981eed71aace56878e5ee4f9df6e7dc494fb5e
DavidMPerich <davidmperich@gmail.com>
2025-06-23 11:28:10 -0700

    add tip field to shaft

--- CueMarket.API/Models/Domain/Shaft.cs
+++ CueMarket.API/Models/Domain/Shaft.cs
@@ -4,9 +4,11 @@
     {
         public Guid Id { get; set; }
         public string Brand { get; set; }
+        public Guid TipId { get; set; }
         public Guid MaterialId { get; set; }
 
         // Navigation Properties
+        public Tip Tip { get; set; }
         public Material Material { get; set; }
     }
 }
