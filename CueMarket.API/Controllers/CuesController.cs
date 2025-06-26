ad51ee72059bbe88df2113b48a48ea00df5a57e8
DavidMPerich <davidmperich@gmail.com>
2025-06-26 11:42:46 -0700

    Add CuesController

---
+++ CueMarket.API/Controllers/CuesController.cs
@@ -0,0 +1,27 @@
+﻿using CueMarket.API.Data;
+using CueMarket.API.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CueMarket.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CuesController : ControllerBase
+    {
+        private readonly CueMarketDbContext dbContext;
+
+        public CuesController(CueMarketDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var cues = dbContext.Cues.ToList();
+
+            return Ok(cues);
+        }
+    }
+}
