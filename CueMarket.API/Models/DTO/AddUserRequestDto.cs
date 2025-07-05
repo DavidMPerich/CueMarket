f9a8481b8392d4a166feada1380759d20650ed78
DavidMPerich <davidmperich@gmail.com>
2025-07-05 16:14:49 -0700

    add model validation to user

--- CueMarket.API/Models/DTO/AddUserRequestDto.cs
+++ CueMarket.API/Models/DTO/AddUserRequestDto.cs
@@ -1,8 +1,13 @@
-﻿namespace CueMarket.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CueMarket.API.Models.DTO
 {
     public class AddUserRequestDto
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "Name has to be at least 3 characters long.")]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
     }
 }
